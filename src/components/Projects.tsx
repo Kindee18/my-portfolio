645ff90cd17c4831e339fe45c288f14f41c6bc82
Kindson Egbule <kindsonegbule15@gmail.com>
2025-10-21 12:10:28 +0100

    Fix Vibe Coding project GitHub link to point to correct my-portfolio repository

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -123,7 +123,7 @@ const projects = [
 		],
 		image:
 			"https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-		link: "https://github.com/Kindee18/portfolio",
+		link: "https://github.com/Kindee18/my-portfolio.git",
 	},
 	{
 		title: "AWS Infrastructure Learning Journey",
